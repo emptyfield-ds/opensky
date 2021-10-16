ab6748969d1c24c68d3085ab93abfef458e3db38
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-10-15 18:51:26 -0700

    add `use_ggplot2_utils()`

---
+++ R/use_ggplot2_utils.R
@@ -0,0 +1,17 @@
+#' Use ggplot2 templates
+#'
+#' Including `theme_minimal2()`, `geom_point2()`, and other variants
+#'
+#' @inheritParams usethis::use_template
+#'
+#' @export
+use_ggplot2_utils <- function(save_as = NULL, open = FALSE) {
+  check_installed("ggplot2")
+
+  if (is.null(save_as)) save_as <- "ggplot2-utils.R"
+  usethis::use_template(
+    "ggplot2-utils.R",
+    save_as = save_as,
+    open = open
+  )
+}
