efb029887154c47ef3b1772d4914649e07c7c3c3
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-10-15 18:57:51 -0700

    add `use_blank_rmd()`

---
+++ R/use_blank_rmd.R
@@ -0,0 +1,14 @@
+#' Use a blank R Markdown file with reasonable settings
+#'
+#' @inheritParams usethis::use_template
+#'
+#' @export
+use_blank_rmd <- function(save_as, open = is_interactive()) {
+  check_installed(c("rmarkdown", "ragg"))
+  usethis::use_template(
+    "blank_rmd.Rmd",
+    save_as = save_as,
+    open = open,
+    package = "opensky"
+  )
+}
