bc4ec95924fffde5a867ae88737b71ba13f61594
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-10-15 18:30:24 -0700

    rename theme

--- R/xaringan.R
+++ R/xaringan.R
@@ -20,19 +20,19 @@ use_xaringan_kakashi <- function(save_as, open = is_interactive()) {
 
 #' @export
 #' @rdname use_xaringan_kakashi
-use_xaringan_elegant <- function(save_as) {
+use_xaringan_tozan <- function(save_as) {
   check_installed(
     c("xaringan", "xaringanthemer", "xaringanExtra", "ragg", "tidyverse")
   )
 
   usethis::use_template(
-    "xaringan_elegant.Rmd",
+    "xaringan_tozan.Rmd",
     save_as = save_as,
     open = open
   )
 
   usethis::use_template(
-    "elegant.css",
+    "tozan.css",
     save_as = path(path_dir(save_as), "theme.css")
   )
 }
